b0ed1ab9b61ea265d10bbbf1b15b91c028a46432
PAN-NikNatGamer <pavniknat@gmail.com>
2024-05-08 11:56:10 +0300

    test.py

--- persing.py
+++ persing.py
@@ -2,21 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 from product import Products
+from clear_lst import clear_file
 import time
+import json
 
+save_lst = []
+clear_file()
+lst_url = ["https://www.perekrestok.ru/cat/search?search=хлеб","https://www.perekrestok.ru/cat/search?search=рыба","https://www.perekrestok.ru/cat/search?search=мясо", "https://www.perekrestok.ru/cat/search?search=молоко"]
 
-product_list = open('product_list.txt','w')
-
+# options.add_argument("--headless")
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
-
-lst_products = []
-# options.add_argument("--headless")
-
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 driver = webdriver.Chrome(options=options)
-
 stealth(driver,
         languages=["en-US", "en"],
         vendor="Google Inc.",
@@ -25,30 +24,18 @@ stealth(driver,
         renderer="Intel Iris OpenGL Engine",
         fix_hairline=True,
         )
+for i in range(len(lst_url)):
+    lst_products = []
+    driver.get(lst_url[i])
+    time.sleep(5)
 
-url = "https://www.perekrestok.ru/cat/search?search=%D1%85%D0%BB%D0%B5%D0%B1"
-driver.get(url)
-time.sleep(5)
+    nazvanie = driver.find_elements(By.CLASS_NAME, 'product-card__title')
+    price = driver.find_elements(By.CLASS_NAME, 'price-new')
 
-
-# Найдем элемент с классом 'product-card__title-wrapper'
-nazvanie = driver.find_elements(By.CLASS_NAME, 'product-card__title')
-price = driver.find_elements(By.CLASS_NAME, 'product-card__price')
-
-print(nazvanie)
-for i in range(len(nazvanie)):
-    #print(nazvanie[i].text)
-    #print(price[i].text)
-    #lst_name.append(f"{name.text}")
-    product_1 = Products(nazvanie[i].text,price[i].text)
-    lst_products.append(product_1)
-    #product_list.write(f"{name.text}\n")
-#for i in range(len(price)):
- #   lst_products[i] = Products("price.text")
-    #lst_price.append(f"{cena.text}")
-    #product_list.write(f"{cena.text}\n")
-
-for i in range(len(lst_products)):
-    print(lst_products[i].name,lst_products[i].price)
+    for i in range(len(nazvanie)):
+        prod = Products(nazvanie[i].text, price[i].text[5:])
+        save_lst.append(prod.to_json())
+        lst_products.append(prod)
+with open("product_list.json", "w") as json_file:
+    json.dump(save_lst, json_file)
 driver.quit()
-product_list.close()
