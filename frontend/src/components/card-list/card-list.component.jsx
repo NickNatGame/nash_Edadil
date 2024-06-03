b850165af2d7b69400e0f2f54399c435c04794b3
ar1zonaranger <tagiroved@mail.ru>
2024-06-03 13:58:05 +0300

    components

--- frontend/src/components/card-list/card-list.component.jsx
+++ frontend/src/components/card-list/card-list.component.jsx
@@ -2,7 +2,17 @@ import {Component} from 'react';
 
 class CardList extends Component{
     render(){
-        return <div>war</div>;
+        console.log(this.props);
+        const {products}=this.props;
+
+        return (
+            <div >
+                {products.map((product)=>(
+                    <h3 key={product.id}>{product.name}</h3>
+                ))}
+                <h1>war</h1>
+            </div>
+        );
     }
 }
 
