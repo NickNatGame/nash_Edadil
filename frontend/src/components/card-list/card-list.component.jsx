5f6cf527fe13e5cc80a340f4885f0ccca4f6e377
ar1zonaranger <tagiroved@mail.ru>
2024-06-05 13:27:07 +0300

    css tweaks; cleanish ctlg&fav

--- frontend/src/components/card-list/card-list.component.jsx
+++ frontend/src/components/card-list/card-list.component.jsx
@@ -3,18 +3,23 @@ import './card-list.styles.css'
 class CardList extends Component{
     render(){
         console.log(this.props);
-        const {products}=this.props;
+        const {products,onAddToCart, onRemoveFromCart, cartCount, onToggleFav}=this.props;
 
         return (
             <div className='card-list'>
                 {products.map((product)=>(
                     <div className='card-container' key={product.id}>
                         <img alt={product.name} src={product.image} height="210" width="210" />
-                        <h3>{product.name}</h3>
+                        <div><h3>{product.name} <button onClick={() => onToggleFav(product)}>♡</button></h3></div>
                         <p> {product.store}: {product.price}₽</p>
+                        <div>
+                            <button onClick={() => onRemoveFromCart(product)}>-</button>
+                            <span>  {cartCount(product)}    </span>
+                            <button onClick={() => onAddToCart(product)}>+</button>
+                        </div>
+                            
                     </div>
-                ))}
-                <h1>war</h1>
+                ))} 
             </div>
         );
     }
